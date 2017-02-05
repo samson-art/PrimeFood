4b99c68a35088962857e4b32ac8267da545ca599
samson-art <samsonov68rus@gmail.com>
2017-02-05 14:12:13 +0300

    auth to demo fix

--- PrimeFood/urls.py
+++ PrimeFood/urls.py
@@ -19,7 +19,7 @@ from . import views as myviews
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^login/$', auth_views.login, {"template_name": "login.html"}, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
