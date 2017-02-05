2eb98c9e6eac64fafd4d15192acc253757764fe2
samson-art <samsonov68rus@gmail.com>
2017-02-05 21:11:23 +0300

    deploy

--- PrimeFood/views.py
+++ PrimeFood/views.py
@@ -12,7 +12,7 @@ def get_content():
     menudata = dict((m.title, {mc.title: {'id': mc.id, 'items': [model_to_dict(mi) for mi in menuitem if mi.menucategory == mc and mi.menucategory.menu == m]} for mc in menucat if mc.menu == m}) for m in menu)
     for m in menu:
         menudata[m.title].update({'id': m.id})
-    return menudata
+    return menudata, menu, menucat, menuitem
 
 
 def landing_page(request):
@@ -26,17 +26,21 @@ def landing_page(request):
             'title': "PrimeFood",
             'slidergallery': Gallery.objects.filter(title='Слайдер').first().photos.all(),
             'gallery': Gallery.objects.filter(title='Галлерея').first().photos.all(),
-            'menudata': get_content()
+            'menudata': get_content()[0]
         })
 
 
 def demo(request):
     if request.user.is_authenticated():
+        menudata, menu, menucat, menuitem = get_content()
         return render(request, 'landing_page.html', {
             'title': 'PrimeFood DEMO',
             'slidergallery': Gallery.objects.filter(title='Слайдер').first().photos.all(),
             'gallery': Gallery.objects.filter(title='Галлерея').first().photos.all(),
-            'menudata': get_content()
+            'menudata': menudata,
+            'menu': menu,
+            'menucats': menucat,
+            'menuitems': menuitem
         })
     else:
         return redirect('landing_page')
