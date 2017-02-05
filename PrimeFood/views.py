0bba9df739424064f0b3b659cc3e00d2cdb9f560
samson-art <samsonov68rus@gmail.com>
2017-02-05 14:39:09 +0300

    auth to demo fix

--- PrimeFood/views.py
+++ PrimeFood/views.py
@@ -23,7 +23,7 @@ def landing_page(request):
                 'menudata': menudata
             })
         else:
-            return redirect('./login')
+            return redirect('/')
     elif datetime.date.today() < datetime.date(2017, 2, 9):
         return render(request, 'timer.html', {
             'start_date': str(datetime.date(2017, 2, 9).strftime('%Y/%m/%d')),
