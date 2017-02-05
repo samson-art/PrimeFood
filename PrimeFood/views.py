d95ee0f1c6a380051cdd5b8cf9d38bb32e842c99
samson-art <samsonov68rus@gmail.com>
2017-02-05 16:22:51 +0300

    deploy

--- PrimeFood/views.py
+++ PrimeFood/views.py
@@ -9,30 +9,34 @@ def landing_page(request):
     menu = Menu.objects.all().order_by('order')
     menucat = MenuCategory.objects.all().order_by('order')
     menuitem = MenuItem.objects.all().order_by('order')
-    menudata = dict((m.title, {mc.title: {'id': mc.id, 'items':[model_to_dict(mi) for mi in menuitem if mi.menucategory == mc and mi.menucategory.menu == m]} for mc in menucat if mc.menu == m}) for m in menu)
+    menudata = dict((m.title, {mc.title: {'id': mc.id, 'items': [model_to_dict(mi) for mi in menuitem if mi.menucategory == mc and mi.menucategory.menu == m]} for mc in menucat if mc.menu == m}) for m in menu)
     for m in menu:
         menudata[m.title].update({'id': m.id})
     sd = request.META['HTTP_HOST'].split(".")
-    if sd[0] == 'demo' or sd[1] == 'demo':
+    if datetime.date.today() < datetime.date(2017, 2, 9):
         if request.user.is_authenticated():
-            return render(request, 'landing_page.html', {
-                'title': 'PrimeFood DEMO',
-                'slidergallery': Gallery.objects.filter(title='Слайдер').first().photos.all(),
-                'gallery': Gallery.objects.filter(title='Галлерея').first().photos.all(),
-                'menudata': menudata
-            })
+            if sd[0] == 'demo' or sd[1] == 'demo':
+                return render(request, 'landing_page.html', {
+                    'title': 'PrimeFood DEMO',
+                    'slidergallery': Gallery.objects.filter(title='Слайдер').first().photos.all(),
+                    'gallery': Gallery.objects.filter(title='Галлерея').first().photos.all(),
+                    'menudata': menudata
+                })
+            else:
+                show_cowntdown(request)
         else:
-            return redirect('http://prime-food.ru')
+            show_cowntdown(request)
     else:
-        if datetime.date.today() < datetime.date(2017, 2, 9):
-            return render(request, 'timer.html', {
-                'start_date': str(datetime.date(2017, 2, 9).strftime('%Y/%m/%d')),
-                'title': 'PrimeFood'
-            })
-        else:
-            return render(request, 'landing_page.html', {
-                'title': "PrimeFood",
-                'slidergallery': Gallery.objects.filter(title='Слайдер').first().photos.all(),
-                'gallery': Gallery.objects.filter(title='Галлерея').first().photos.all(),
-                'menudata': menudata
-            })
+        return render(request, 'landing_page.html', {
+            'title': "PrimeFood",
+            'slidergallery': Gallery.objects.filter(title='Слайдер').first().photos.all(),
+            'gallery': Gallery.objects.filter(title='Галлерея').first().photos.all(),
+            'menudata': menudata
+        })
+
+
+def show_cowntdown(request):
+    return render(request, 'timer.html', {
+        'start_date': str(datetime.date(2017, 2, 9).strftime('%Y/%m/%d')),
+        'title': 'PrimeFood'
+    })
