d1a3aeec78792bba30a2b8554635c7b8e4262d8a
samson-art <samsonov68rus@gmail.com>
2017-02-05 13:20:39 +0300

    demo fix

--- PrimeFood/views.py
+++ PrimeFood/views.py
@@ -13,10 +13,8 @@ def landing_page(request):
     menudata = dict((m.title, {mc.title: {'id': mc.id, 'items':[model_to_dict(mi) for mi in menuitem if mi.menucategory == mc and mi.menucategory.menu == m]} for mc in menucat if mc.menu == m}) for m in menu)
     for m in menu:
         menudata[m.title].update({'id': m.id})
-
-    django_site = Site.objects.get_current()
-    domain_parts = django_site.domain.split(".")[1]
-    if domain_parts == 'demo':
+    sd = Site.objects.get_current().domain.split(".")[1]
+    if sd == 'demo':
         return render(request, 'landing_page.html', {
             'title': 'PrimeFood DEMO',
             'slidergallery': Gallery.objects.filter(title='Слайдер').first().photos.all(),
@@ -26,7 +24,8 @@ def landing_page(request):
     if datetime.date.today() < datetime.date(2017, 2, 9):
         return render(request, 'timer.html', {
             'start_date': str(datetime.date(2017, 2, 9).strftime('%Y/%m/%d')),
-            'title': 'PrimeFood'
+            'title': 'PrimeFood',
+            'sd': sd
         })
     else:
         return render(request, 'landing_page.html', {
