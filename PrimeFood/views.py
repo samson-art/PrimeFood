98471274286a619d0f98fcce5d33775805545cde
samson-art <samsonov68rus@gmail.com>
2017-02-05 13:57:50 +0300

    demo fix

--- PrimeFood/views.py
+++ PrimeFood/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.forms.models import model_to_dict
 from photologue.models import Gallery
 from .models import *
 import datetime
-#
+from django.contrib.auth import views as auth_views
 
 
 def landing_page(request):
@@ -15,12 +15,15 @@ def landing_page(request):
         menudata[m.title].update({'id': m.id})
     sd = request.META['HTTP_HOST'].split(".")
     if sd[0] == 'demo' or sd[1] == 'demo':
-        return render(request, 'landing_page.html', {
-            'title': 'PrimeFood DEMO',
-            'slidergallery': Gallery.objects.filter(title='Слайдер').first().photos.all(),
-            'gallery': Gallery.objects.filter(title='Галлерея').first().photos.all(),
-            'menudata': menudata
-        })
+        if request.user.is_authenticated():
+            return render(request, 'landing_page.html', {
+                'title': 'PrimeFood DEMO',
+                'slidergallery': Gallery.objects.filter(title='Слайдер').first().photos.all(),
+                'gallery': Gallery.objects.filter(title='Галлерея').first().photos.all(),
+                'menudata': menudata
+            })
+        else:
+            return redirect(auth_views)
     elif datetime.date.today() < datetime.date(2017, 2, 9):
         return render(request, 'timer.html', {
             'start_date': str(datetime.date(2017, 2, 9).strftime('%Y/%m/%d')),
