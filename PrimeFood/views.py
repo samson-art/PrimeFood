2cf36d8f321c68e7f859d852a52227e4a15e9fe0
samson-art <samsonov68rus@gmail.com>
2017-02-05 04:02:06 +0300

    Timer template

--- PrimeFood/views.py
+++ PrimeFood/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.forms.models import model_to_dict
 from photologue.models import Gallery
 from .models import *
-
+import datetime
 
 def landing_page(request):
     menu = Menu.objects.all().order_by('order')
@@ -11,9 +11,15 @@ def landing_page(request):
     menudata = dict((m.title, {mc.title: {'id': mc.id, 'items':[model_to_dict(mi) for mi in menuitem if mi.menucategory == mc and mi.menucategory.menu == m]} for mc in menucat if mc.menu == m}) for m in menu)
     for m in menu:
         menudata[m.title].update({'id': m.id})
-    return render(request, 'landing_page.html', {
-        'title': "PrimeFood",
-        'slidergallery': Gallery.objects.filter(title='Слайдер').first().photos.all(),
-        'gallery': Gallery.objects.filter(title='Галлерея').first().photos.all(),
-        'menudata': menudata
-    })
+    if datetime.datetime.now() < datetime.date(2016, 2, 5):
+        return render(request, 'timer.html', {
+            'start_date': datetime.today(),
+            'title': 'PrimeFood'
+        })
+    else:
+        return render(request, 'landing_page.html', {
+            'title': "PrimeFood",
+            'slidergallery': Gallery.objects.filter(title='Слайдер').first().photos.all(),
+            'gallery': Gallery.objects.filter(title='Галлерея').first().photos.all(),
+            'menudata': menudata
+        })
