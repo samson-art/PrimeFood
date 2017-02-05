319c62f46991c8ede5e7cc061f18a6bb00990d3d
samson-art <samsonov68rus@gmail.com>
2017-02-05 17:30:05 +0300

    deploy

--- PrimeFood/admin.py
+++ PrimeFood/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from .models import Menu, MenuCategory, MenuItem
 
+
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(MenuCategory)
 class MenuCategoryAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
