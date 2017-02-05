1c477bce9976ed7a6d3f09e5cb817a38fccccf0e
samson-art <samsonov68rus@gmail.com>
2017-02-05 11:37:45 +0300

    ew

--- PrimeFood/settings.py
+++ PrimeFood/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2imq82yyp^8ma0_)pnihs--o5u&m8sb1!d8(c0@#=v=+3pp1c3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
